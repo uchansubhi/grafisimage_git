7859d489447cd45313c03334941687f166715bc5
Uchan <uchansubhi@gmail.com>
2019-11-01 21:11:43 +0700

    SOAL7_1

---
+++ app/src/main/java/com/example/soal7_1/SOAL7_1.java
@@ -0,0 +1,52 @@
+package com.example.soal7_1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+public class SOAL7_1 extends Activity {
+    private Button tekan;
+    private GLSurfaceView grafik1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soal7_1);
+        tekan=(Button)findViewById(R.id.button1);
+        grafik1 = new HelloESSurfaceView(this);
+        setContentView(grafik1);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        grafik1.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        grafik1.onResume();
+    }
+
+    class HelloESSurfaceView extends GLSurfaceView{
+        public HelloESSurfaceView(Context context){
+            super(context);
+
+            setRenderer(new segitiga());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_latih7_grafik_klasik, menu);
+        return true;
+
+    }
+}
